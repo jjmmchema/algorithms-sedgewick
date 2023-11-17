bb1a8a3f7fa5b60e369a56a7b0189fdf45427dde
Josemaria <marinmjosemaria@gmail.com>
2023-11-16 19:55:27 -0500

    Finished Percolation assignment

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -8,17 +8,19 @@ public class Percolation {
     private final int virtualBotId;
     private final WeightedQuickUnionUF weightedQU;
 
-    public final boolean[] state; // True for open, False for full
+    private final boolean[] state; // True for open, False for full
 
     public Percolation(int n) {
         if (n < 1)
             throw new IllegalArgumentException();
         this.gridSize = n;
         noOfOpen = 0;
-        virtualTopId = n*n;
-        virtualBotId = n*n+1;
-        weightedQU = new WeightedQuickUnionUF((n*n)+2); // Add two for a 'virtual' node at the top and the bottom
-        state = new boolean[n*n];
+        virtualTopId = n * n;
+        virtualBotId = n * n + 1;
+        weightedQU = new WeightedQuickUnionUF((n * n) + 2); // Add two for a 'virtual' node at the top and the bottom
+        state = new boolean[n * n + 2];
+        state[virtualTopId] = true;
+        state[virtualBotId] = true;
     }
 
     public void open(int row, int col) {
@@ -28,12 +30,14 @@ public class Percolation {
         int nodeId = map2dTo1d(row, col);
 
         if (col != 1) {
-            int leftNode = map2dTo1d(row, col-1);
-            this.weightedQU.union(nodeId, leftNode);
+            int leftNode = map2dTo1d(row, col - 1);
+            if (state[leftNode])
+                this.weightedQU.union(nodeId, leftNode);
         }
         if (col != this.gridSize) {
-            int rightNode = map2dTo1d(row, col-1);
-            this.weightedQU.union(nodeId, rightNode);
+            int rightNode = map2dTo1d(row, col + 1);
+            if (state[rightNode])
+                this.weightedQU.union(nodeId, rightNode);
         }
 
         // Assume that these are true at first
@@ -41,33 +45,37 @@ public class Percolation {
         int botNode = virtualBotId;
 
         if (row != 1) {
-            topNode = map2dTo1d(row-1, col);;
+            topNode = map2dTo1d(row - 1, col);
         }
         if (row != this.gridSize) {
-            botNode = map2dTo1d(row+1, col);;
+            botNode = map2dTo1d(row + 1, col);
         }
 
-        this.weightedQU.union(nodeId, botNode);
-        this.weightedQU.union(nodeId, topNode);
+        if (state[botNode])
+            this.weightedQU.union(nodeId, botNode);
+        if (state[topNode])
+            this.weightedQU.union(nodeId, topNode);
 
-        this.state[nodeId-1] = true;
+        this.state[nodeId] = true;
         this.noOfOpen++;
     }
 
     public boolean isOpen(int row, int col) {
         checkValidIdx(row, col);
         int id = map2dTo1d(row, col);
-        return state[id-1];
+        return state[id];
     }
 
     public boolean isFull(int row, int col) {
-        return !isOpen(row, col);
+        checkValidIdx(row, col);
+        return weightedQU.find(map2dTo1d(row, col)) == weightedQU.find(virtualTopId);
     }
 
-    public int numberOfOpenSites() { return this.noOfOpen; }
+    public int numberOfOpenSites() {
+        return this.noOfOpen;
+    }
 
     public boolean percolates() {
-        // weightedQU.connected(virtualTopId, virtualBotId) // Deprecated??
         return weightedQU.find(virtualTopId) == weightedQU.find(virtualBotId);
     }
 
@@ -76,29 +84,11 @@ public class Percolation {
             throw new IllegalArgumentException();
     }
 
-    public int map2dTo1d(int row, int col) {
-        int zeroIdxRow = row-1;
-        int zeroIdxCol = col-1;
-        return (zeroIdxRow*this.gridSize + zeroIdxCol) + 1;
+    private int map2dTo1d(int row, int col) {
+        int zeroIdxRow = row - 1;
+        int zeroIdxCol = col - 1;
+        return (zeroIdxRow *  this.gridSize + zeroIdxCol);
     }
 
-    public static void main(String[] args) {
-        Percolation percolation = new Percolation(1);
-        System.out.println(percolation.percolates());
-        percolation.open(1, 1);
-        System.out.println(percolation.percolates());
-
-        percolation = new Percolation(3);
-        System.out.println(percolation.noOfOpen);
-        percolation.open(1, 1);
-        System.out.println(percolation.noOfOpen);
-        System.out.println(percolation.isOpen(1, 1));
-
-        System.out.println(percolation.percolates());
-        percolation.open(1, 2);
-        percolation.open(2, 2);
-        percolation.open(3, 2);
-        System.out.println(percolation.percolates());
-
-    }
+    public static void main(String[] args) {  }
 }
--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -1,14 +1,63 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
 
-    public PercolationStats(int n, int trials) {}
+    private static final double CONFIDENCE_95 = 1.96;
+    private final int trials;
+    private final double[] thresholds;
 
-    public double mean() { return 0.0; }
+    public PercolationStats(int n, int trials) {
+        if (n < 1 || trials < 1)
+            throw new IllegalArgumentException();
 
-    public double stddev() { return 0.0; }
+        this.trials = trials;
+        thresholds = new double[trials];
 
-    public double confidenceLo() { return 0.0; }
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            while (!p.percolates()) {
+                int randRow = StdRandom.uniformInt(1, n + 1);
+                int randCol = StdRandom.uniformInt(1, n + 1);
+                p.open(randRow, randCol);
+            }
+            thresholds[i] = p.numberOfOpenSites() / ((double) (n * n));
+        }
 
-    public double confidenceHi() { return 0.0; }
+    }
 
-    public static void main(String[] args) {}
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    public double confidenceLo() {
+        double b = (CONFIDENCE_95 * stddev()) / Math.sqrt(trials);
+        return mean() - b;
+    }
+
+    public double confidenceHi() {
+        double b = (CONFIDENCE_95 * stddev()) / Math.sqrt(trials);
+        return mean() + b;
+    }
+
+    private String printResults() {
+        return String.format("mean \t\t\t\t\t = %f \nstddev \t\t\t\t\t = %f \n95%% confidence interval  = [%f, %f]",
+                mean(),
+                stddev(),
+                confidenceLo(),
+                confidenceHi());
+    }
+
+    // ALT+SHIFT+F10 to change cli arguments
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, trials);
+//        ps.runMonteCarloSim();
+        System.out.println(ps.printResults());
+    }
 }
